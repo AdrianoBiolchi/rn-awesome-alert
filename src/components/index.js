99f6ec56413ee5d4e4d93b9ac5dd3115de200c0f
Adriano Biolchi <adrianobiolchi@outlook.com>
2020-07-07 14:12:50 -0300

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import SCLAlert from './SCLAlert'
+import SCLAlertHeader from './SCLAlertHeader'
+import SCLAlertButton from './SCLAlertButton'
+import SCLAlertTitle from './SCLAlertTitle'
+import SCLAlertSubtitle from './SCLAlertSubtitle'
+
+export { SCLAlert, SCLAlertHeader, SCLAlertButton, SCLAlertTitle, SCLAlertSubtitle }
